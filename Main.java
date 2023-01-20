186648392c2ec7704931918a47d853d971f331d2
tekater <120014627+tekater@users.noreply.github.com>
2023-01-20 20:45:53 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,8 +7,9 @@ public class Main {
         System.out.println("Введите значение X: ");
         double x = scanner.nextDouble(); // 22.5
         double y = Math.pow(x , 3) + Math.pow(x , 2) + x + 1;
+        double z = 1 + x * (1 + x * (1 + x)); // Второй вариант формулы
         System.out.println("Значение Y: ");
         System.out.println(y); // 11920.375
-
+        
     }
 }
